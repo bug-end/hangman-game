0f866d739e9809f1940f8a44c5b482256c41987f
lucas <l.bielecki13@gmail.com>
2021-10-15 01:34:17 +0200

    adding span elements for letters

--- script.js
+++ script.js
@@ -4,11 +4,36 @@ const gallows = document.querySelector('.gallows');
 const answer = document.querySelector('.answer_box');
 const letters = document.querySelector('.letters');
 const reset = document.querySelector('.reset_button');
-// const oneletter = document.querySelectorAll('.letters__item');
 
 submit.addEventListener('click', function() {
-	const word = input.value.split('');
-	// console.log(word, word[3]);
 	this.disabled = true;
+	let puzzle = input.value;
+	let arrayOfLetters = puzzle.split('');
+
+	// funkcja stwarzająca miejsca na litery
+	function createLetterSpace() {
+		for (let i=0; i < puzzle.length; i++) {
+			let span = document.createElement('span');
+			span.className = 'note';
+			// span.innerHTML = '_';
+			
+			document.querySelector('.answer_box').appendChild(span);
+		}
+	}
+	createLetterSpace()
+
+	letters.addEventListener('click', event => {
+		if (!event.target.closest('button')) return // zapobiega wyświetlaniu wszystkich liter po kliknięciu w div letter
+		const letter = event.target.textContent;
+		
+		for (let i=0; i < arrayOfLetters.length; i++) {
+			if (arrayOfLetters[i] === letter) {
+				return console.log(true);
+
+			}
+		}
+		
+	})
+
 });
 
