bf53e4835a818657f5a29ab0d6ce3c62fbc5751c
lucas <l.bielecki13@gmail.com>
2021-11-19 19:29:41 +0100

    working on animations and style

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
 const input = document.getElementById('word');
 const submit = document.getElementById('submit_button');
 const gallows = document.querySelector('.gallows');
+const gallAnimation = document.querySelector('.gall_anim');
 const transparentBox = document.querySelector('.transparent_box');
-const blur = document.querySelector('.blur');
 const face = document.querySelector('.face');
+const popOver = document.querySelector('.popover');
+const alphabet = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ';
+const letter = alphabet.split('');
+const paper = document.querySelector('.paper');
+let letbox = document.getElementById('letter_box');
 let mistakes = 0;
 let puzzle; // zmienna globalna z wartością przypisywaną po kliknięciu w przycisk submit
 let blanks = '';
-const alphabet = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ';
-const letter = alphabet.split('');
 
 submit.addEventListener('click', function() {
 	puzzle = input.value;
 	puzzle = puzzle.toUpperCase();
-	console.log(puzzle);
-	for (let i = 0; i < puzzle.length; i++) {
-		if (puzzle.charAt(i)==' ') {
-			blanks = blanks + ' ';
-		} else {
-			blanks = blanks + '-';
-		}
-	};
-	submit.disabled = true;
-	transparentBox.style.display = 'none';
-	blur.classList.remove('blur');
-	blanksGenerator()
-})
+	let regex = /^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+( [a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+)*$/;
+	if(input.value.match(regex)) {
+      for (let i = 0; i < puzzle.length; i++) {
+			if (puzzle.charAt(i)==' ') {
+				blanks = blanks + ' ';
+			} else {
+				blanks = blanks + '-';
+			}
+		};
+		transparentBox.style.display = 'none';
+		gallows.classList.remove('display');
+		gallAnimation.classList.add('animate__backInLeft');
+		paper.classList.add('animate__rollIn', 'animate__delay-1s');
+		letbox.classList.add('animate__backInRight');
+		blanksGenerator()
+		lettersGenerator()
+      } else {
+      		popOver.classList.remove('display');
+      		setTimeout(function(){ popOver.classList.add('display'); }, 3000);
+      		return false;
+      	};
+});
 
 function blanksGenerator() {
 	document.getElementById('answer_box').innerHTML = blanks;
 };
-
-window.onload = lettersGenerator;
 
 // funkcja generująca klawiaturę
 function lettersGenerator() {
 	let letterbox = '';
 	for (let i = 0; i < letter.length; i++) {
 		let element = 'l' + i;
-		letterbox = letterbox + '<div class="letter" onclick="checkLetter(' + i + ')" id="' + element + '" >' +	letter[i] + '</div>';
+		letterbox = letterbox + '<div class="letter hover" onclick="checkLetter(' + i + ')" id="' + element + '" >' +	letter[i] + '</div>';
 		}
-	document.getElementById('letter_box').innerHTML = letterbox;
+	letbox.innerHTML = letterbox;
 };
 
 // utworzenie metody changeBlank zmieniającej znak pod wskazaną pozycją w stringu
@@ -66,11 +76,13 @@ function checkLetter(nr) {
 		let element = 'l' + nr;
 		let elem2 = document.getElementById(element);
 		elem2.classList.add('green');
+		elem2.classList.remove('hover');
 		blanksGenerator();
 	} else {
 		let element = 'l' + nr;
 		let elem2 = document.getElementById(element);
 		elem2.classList.add('red');
+		elem2.classList.remove('hover');
 		elem2.setAttribute('onclick', ';');
 
 		// zła litera
@@ -81,14 +93,13 @@ function checkLetter(nr) {
 
 	// wygrana
 	if (puzzle == blanks) {
-		document.getElementById('letter_box').innerHTML = '<p>Wygrałeś!</p>' + '<p class="reset" onclick="location.reload()">ZAGRAJ JESZCZE RAZ</p>';
+		letbox.innerHTML = '<p class="reset" onclick="location.reload()">WYGRAŁEŚ! TO CO, JESZCZE RAZ?</p>';
 	}
 
 	// przegrana
 	if (mistakes >= 7) {
-
 		face.style.display = 'block';
-		document.getElementById('letter_box').innerHTML = '<p class="reset" onclick="location.reload()">ZAGRAJ JESZCZE RAZ</p>';
+		letbox.innerHTML = '<p class="reset" onclick="location.reload()">GŁOWA DO GÓRY<br>SPRÓBUJ JESZCZE RAZ</p>';
 	}
 
 };
